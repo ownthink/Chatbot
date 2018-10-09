e98769d0e6a8fd90646484783fc9de076d6235bd
Yener <help@ownthink.com>
2018-10-09 11:23:50 +0800

    update

--- bot.py
+++ bot.py
@@ -1,10 +1,8 @@
-import requests
+from chat import client
 
-url = 'https://api.ownthink.com/bot?info=你好'      # 口语理解API
-sess = requests.get(url) # 请求
+if __name__=='__main__':
+	question = '你好'
 
-text = sess.text # 获取返回的数据
-print(text) 
-
-text = eval(text) # 转为字典类型
-print(text)
+	answer = client.get_answer(question)
+	
+	print(answer)
--- chat/client.py
+++ chat/client.py
@@ -1,1 +1,12 @@
 import requests
+
+def get_answer(text):
+	url = 'https://api.ownthink.com/bot?info=%s'%text
+	sess = requests.get(url)
+
+	text = sess.text
+	
+	text = eval(text)
+	
+	
+	return text
